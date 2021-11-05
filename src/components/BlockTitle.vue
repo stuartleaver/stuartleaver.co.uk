1dcd0a0a9953553d00cdda1d6423f48c8b9b5c3e
stuartleaver <stuartleaveruk@gmail.com>
2021-11-05 18:56:41 +0000

    Create reusable block title

---
+++ src/components/BlockTitle.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="block-title">
+    <h3>{{ title }}</h3>
+  </div>
+</template>
+<script>
+export default {
+  name: "BlockTitle",
+  props: {
+    title: String,
+  },
+};
+</script>
+<style lang="postcss" scoped>
+.block-title {
+  @apply flex left-0 mt-1 mb-3 text-xl font-semibold;
+}
+
+.block-title h3 {
+  @apply relative text-left;
+}
+
+.block-title h3:after {
+  @apply absolute right-0 left-0 w-auto bottom-0 mx-auto bg-blue-500;
+  content: "";
+  height: 2px;
+  -webkit-transition: all 0.15s ease-in-out;
+  -moz-transition: all 0.15s ease-in-out;
+  -o-transition: all 0.15s ease-in-out;
+  -ms-transition: all 0.15s ease-in-out;
+  transition: all 0.15s ease-in-out;
+}
+</style>

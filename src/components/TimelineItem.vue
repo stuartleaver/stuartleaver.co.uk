0c36860371a7c717b1fdcae2599aafddfa1a2e16
stuartleaver <stuartleaveruk@gmail.com>
2021-11-07 17:18:18 +0000

    Refeactor the timeline item to be a reusable component

---
+++ src/components/TimelineItem.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="timeline-item">
+    <h4 class="item-title">{{ timelineItem.title }}</h4>
+    <span class="item-date">{{ timelineItem.date }}</span>
+    <span class="item-company">{{ timelineItem.company }}</span>
+    <p class="item-desctiption">
+      {{ timelineItem.description }}
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  name: "TimelineItem",
+  props: {
+    timelineItem: Object,
+  },
+};
+</script>
+<style lang="postcss" scoped>
+.timeline-item {
+  @apply relative px-5 py-4 ml-5 mb-5 border-l-2 border-blue-500 shadow-timeline text-left;
+}
+
+.timeline-item:last-child {
+  @apply mb-0;
+}
+
+.timeline-item:before {
+  @apply inline-block absolute bg-white rounded-xl w-3 h-3 top-5 -left-7/5 border-2 border-blue-500;
+  content: "";
+  z-index: 2;
+}
+
+.timeline-item:after {
+  @apply inline-block absolute bg-blue-500 rounded-xl w-7 h-0.5 top-6 -left-7;
+  content: "";
+  z-index: 1;
+}
+
+.timeline-item .item-title {
+  @apply text-base font-semibold;
+}
+
+.timeline-item .item-date {
+  @apply text-xs text-blue-500 mb-2;
+}
+
+.timeline-item .item-company {
+  @apply text-xs text-gray-500 ml-2 mb-2 pl-2  border-l-2;
+}
+
+.timeline-item p {
+  @apply text-sm;
+}
+</style>
--- src/views/CV.vue
+++ src/views/CV.vue
@@ -10,15 +10,7 @@
             <div class="block-content">
               <BlockTitle title="Education" />
               <div class="timeline">
-                <div class="timeline-item">
-                  <h4 class="item-title">Lorem Ipsum</h4>
-                  <span class="item-date">2021</span>
-                  <span class="item-company">Microsoft</span>
-                  <p class="item-desctiption">
-                    Neque porro quisquam est qui dolorem ipsum quia dolor sit
-                    amet, consectetur, adipisci velit...
-                  </p>
-                </div>
+                <TimelineItem :timeline-item="this.timelineItem"/>
               </div>
             </div>
             <div class="block-content">
@@ -32,11 +24,23 @@
 </template>
 <script>
 import BlockTitle from "@/components/BlockTitle.vue";
+import TimelineItem from "@/components/TimelineItem.vue";
 
 export default {
   name: "CV",
   components: {
     BlockTitle,
+    TimelineItem,
+  },
+  data() {
+    return {
+      timelineItem: {
+        title: "Bachelor of Science in Computer Science",
+        date: "2011 - 2015",
+        company: "University of Waterloo",
+        description: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+      }
+    };
   },
 };
 </script>
@@ -67,41 +71,5 @@ export default {
 
 .timeline {
   @apply py-4 border-l-2;
-}
-
-.timeline-item {
-  @apply relative px-5 py-4 ml-5 mb-5 border-l-2 border-blue-500 shadow-timeline text-left;
-}
-
-.timeline-item:last-child {
-  @apply mb-0;
-}
-
-.timeline-item:before {
-  @apply inline-block absolute bg-white rounded-xl w-3 h-3 top-5 -left-7/5 border-2 border-blue-500;
-  content: "";
-  z-index: 2;
-}
-
-.timeline-item:after {
-  @apply inline-block absolute bg-blue-500 rounded-xl w-7 h-0.5 top-6 -left-7;
-  content: "";
-  z-index: 1;
-}
-
-.timeline-item .item-title {
-  @apply text-base font-semibold;
-}
-
-.timeline-item .item-date {
-  @apply text-xs text-blue-500 mb-2;
-}
-
-.timeline-item .item-company {
-  @apply text-xs text-gray-500 ml-2 mb-2 pl-2  border-l-2;
-}
-
-.timeline-item p {
-  @apply text-sm;
 }
 </style>

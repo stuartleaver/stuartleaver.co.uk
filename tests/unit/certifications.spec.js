f1d954c3c2bf9839ad4ba40d58fd2b3d34acb3b8
stuartleaver <stuartleaveruk@gmail.com>
2021-11-15 20:27:02 +0000

    Refactor tests to mock axios

--- tests/unit/certifications.spec.js
+++ tests/unit/certifications.spec.js
@@ -1,13 +1,33 @@
 import { shallowMount } from "@vue/test-utils";
 import Certifications from "@/views/Certifications.vue";
+import axios from "axios";
+
+jest.mock("axios");
+
+const certifications = {
+  azure: {
+    title: "Microsoft Azure",
+    certifications: [
+      {
+        title: "title",
+        image: "image",
+        imageAlt: "imageAlt",
+      },
+    ],
+  },
+};
 
 describe("Certifications.vue", () => {
   it("renders componant", () => {
+    axios.get.mockResolvedValueOnce(certifications);
+
     const wrapper = shallowMount(Certifications);
     expect(wrapper.exists()).toBe(true);
   });
 
   it("renders the title element", () => {
+    axios.get.mockResolvedValueOnce(certifications);
+
     const wrapper = shallowMount(Certifications);
     expect(wrapper.find(".page-header h2").text()).toEqual("Certifications");
   });
